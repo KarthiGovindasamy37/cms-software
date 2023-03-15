7a6586d20e3e2dec1f8ae7f5fa707f570e515e68
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2023-03-15 18:26:42 +0530

    Updated

--- src/Component/Topbar.jsx
+++ src/Component/Topbar.jsx
@@ -3,13 +3,13 @@ import { useSelector } from 'react-redux'
 
 function Topbar() {
 
-  const{name}=useSelector(state=>state.user.user)
-  const userName= name.toUpperCase()
+  // const{name}=useSelector(state=>state.user.user)
+  // const userName= name.toUpperCase()
   return (
     <div className='topbar'>
       <div className='topbar-height d-flex align-items-end justify-content-end'>
       <h4 className=''>Welcome</h4>
-      <h3 className='ps-2 pe-2'>{userName}</h3>
+      <h3 className='ps-2 pe-2'>Karthi</h3>
       </div>
         
     </div>
